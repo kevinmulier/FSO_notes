78c9b277dd99ad87d24b4eea5772eae9d89967d2
kevinmulier <kevin.mulier@outlook.fr>
2024-01-15 15:45:34 +0100

    add new post and single post fetching

--- back/index.js
+++ back/index.js
@@ -31,39 +31,28 @@ app.get("/api/notes", (request, response) => {
 });
 
 app.get("/api/notes/:id", (request, response) => {
-	const id = Number(request.params.id);
-	const note = notes.find((note) => note.id === id);
-
-	if (note) {
+	Note.findById(request.params.id).then((note) => {
 		response.json(note);
-	} else {
-		response.statusMessage = `The ressource with id ${id} was not found`;
-		response.status(400).end();
-	}
+	});
 });
-
-const generateId = () => {
-	const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
-	return maxId + 1;
-};
 
 app.post("/api/notes", (request, response) => {
 	const body = request.body;
+
 	if (!body.content) {
 		return response.status(400).json({
 			error: "content missing",
 		});
 	}
 
-	const note = {
+	const note = new Note({
 		content: body.content,
 		important: Boolean(body.important) || false,
-		id: generateId(),
-	};
+	});
 
-	notes = notes.concat(note);
-
-	response.json(note);
+	note.save().then((savedNote) => {
+		response.json(savedNote);
+	});
 });
 
 app.delete("/api/notes/:id", (request, response) => {
