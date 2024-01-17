a7d1322e39de8b9f3f0885e4237500fb336dce08
kevinmulier <kevin.mulier@outlook.fr>
2024-01-17 15:05:52 +0100

    fix linting

--- back/.eslintrc.js
+++ back/.eslintrc.js
@@ -21,13 +21,13 @@ module.exports = {
   },
   rules: {
     'indent': ['error', 2],
-    'linebreak-style': ['error', 'unix'],
+    'linebreak-style': ['error', 'windows'],
     'quotes': ['error', 'single'],
-    'semi': ['error', 'never'],
+    'semi': ['error', 'always'],
     'eqeqeq': 'error',
     'no-trailing-spaces': 'error',
     'object-curly-spacing': ['error', 'always'],
     'arrow-spacing': ['error', { before: true, after: true }],
     'no-console': 0,
   },
-}
+};
