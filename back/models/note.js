7510987c83948056d09cd5e20b3f97bf35f22676
kevinmulier <kevin.mulier@outlook.fr>
2024-01-25 17:44:43 +0100

    add references between schema

--- back/models/note.js
+++ back/models/note.js
@@ -1,17 +1,20 @@
 const mongoose = require('mongoose');
 
-const noteSchema = new mongoose.Schema({
-  content: {
-    type: String,
-    required: true,
-    minlength: 5,
+const noteSchema = new mongoose.Schema(
+  {
+    content: {
+      type: String,
+      required: true,
+      minlength: 5,
+    },
+    important: Boolean,
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+    },
   },
-  important: Boolean,
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-  },
-});
+  { timestamps: true },
+);
 
 noteSchema.set('toJSON', {
   transform: (document, returnedObject) => {
