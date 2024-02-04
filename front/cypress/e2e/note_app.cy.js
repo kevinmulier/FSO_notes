43f7a753dfb062ad5777e5a068465da9d83713bc
kevinmulier <kevin.mulier@outlook.fr>
2024-02-04 21:29:16 +0100

    add E2E test to ensure that logged user can add a new note

--- front/cypress/e2e/note_app.cy.js
+++ front/cypress/e2e/note_app.cy.js
@@ -15,5 +15,23 @@ describe('Note app', function () {
     cy.get('#username').type('mluukkai');
     cy.get('#password').type('salainen');
     cy.get('#login-button').click();
+
+    cy.contains('testuser logged in');
+  });
+
+  describe('when logged in', function () {
+    this.beforeEach(function () {
+      cy.contains('log in').click();
+      cy.get('#username').type('mluukkai');
+      cy.get('#password').type('salainen');
+      cy.get('#login-button').click();
+    });
+
+    it('a new note can be created', function () {
+      cy.contains('new note').click();
+      cy.get('#note-input').type('a note created by cypress');
+      cy.contains('save').click();
+      cy.contains('a note created by cypress');
+    });
   });
 });
